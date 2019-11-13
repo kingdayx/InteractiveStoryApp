3d0b9d935197f18cbf5dea930473e6d64508f4fa
kingdayx <54965488+kingdayx@users.noreply.github.com>
2019-11-13 14:50:32 -0500

    added images

--- app/src/main/java/com/daotec/Interactivestoryapp/MainActivity.java
+++ app/src/main/java/com/daotec/Interactivestoryapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.daotec.Interactivestoryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText nameField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameField = (EditText)  findViewById(R.id.nameEditText);
     }
 }
